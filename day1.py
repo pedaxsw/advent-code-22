7a836cda9d43cfdaad39a4cbc5e02ead3d6dd50b
alex <alexmmail.gmail.com>
2023-01-24 16:28:44 +0100

    make input file, describe how to make code

---
+++ day1.py
@@ -0,0 +1,15 @@
+hodnoty = open("day1_input", "r")
+print(hodnoty.readline())
+
+
+elfs = [
+    #here add each elf and add his calories to the list
+]
+    #here calc the elf with most calories
+    # using max function find elf with most calories
+    #print answer
+for hodnoty in range (-1):
+    cal = print(hodnoty.readline())
+    elfread = cal
+    #here define how to seperate data or each elf, after
+    #finding a blank line = end of elf1 and make elf2 with empty list
