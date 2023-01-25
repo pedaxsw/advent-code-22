2608eb6c91f5bcbdc13179d10cd2c31e1e076154
alex <alexmmail.gmail.com>
2023-01-25 20:11:51 +0100

    working list of numbers

--- day1.py
+++ day1.py
@@ -1,5 +1,7 @@
 with open("day1_input", "r") as file:
-    hodnoty = file.readlines()
+    hodnoty = file.read()
+cislo = hodnoty.split()
+print(cislo)
 count = 0
 
 elfs = [
@@ -8,15 +10,14 @@ elfs = [
     #here calc the elf with most calories
     # using max function find elf with most calories
     #print answer
-n = "\n"
-line = elfs.count(n)
+
+
 for ciselka in hodnoty:
-    if line == '\n':
+    if hodnoty == '\n':
         elfs.append(f"elf{count}")
         count +=1
     else:
         elfs.insert(count, hodnoty[count])
 
-print(elfs)
     #here define how to seperate data or each elf, after
     #finding a blank line = end of elf1 and make elf2 with empty list
