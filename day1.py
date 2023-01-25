b77be3602bdaf8e9a730dbf5017f646963178aa3
alex <alexmmail.gmail.com>
2023-01-25 19:32:45 +0100

    better reading from data

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
-hodnoty = open("day1_input", "r")
-print(hodnoty.readline())
-
+with open("day1_input", "r") as file:
+    hodnoty = file.readlines()
+count = 0
 
 elfs = [
     #here add each elf and add his calories to the list
@@ -8,8 +8,15 @@ elfs = [
     #here calc the elf with most calories
     # using max function find elf with most calories
     #print answer
-for hodnoty in range (-1):
-    cal = print(hodnoty.readline())
-    elfread = cal
+n = "\n"
+line = elfs.count(n)
+for ciselka in hodnoty:
+    if line == '\n':
+        elfs.append(f"elf{count}")
+        count +=1
+    else:
+        elfs.insert(count, hodnoty[count])
+
+print(elfs)
     #here define how to seperate data or each elf, after
     #finding a blank line = end of elf1 and make elf2 with empty list
